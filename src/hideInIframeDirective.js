e8908ed1118342c857b4c1bf849ae8542708f364
Mark Gil Libres <markglibres@gmail.com>
2015-09-01 12:49:23 +0400

    fixed spacing

--- src/hideInIframeDirective.js
+++ src/hideInIframeDirective.js
@@ -1,13 +1,10 @@
 ﻿'use strict'
-
 define(['app'], function (app) {
     app.directive('hideInIframe', [function () {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
-
-                if (self != top)
-                {
+                if (self != top){
                     element.addClass('ng-hide');
                 }
             }
