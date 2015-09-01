618929ed0cdf378c526e0abde7a8e86d0f4e6d65
Mark Gil Libres <markglibres@gmail.com>
2015-09-01 12:44:16 +0400

    initial commit
    initial commit

---
+++ src/hideInIframeDirective.js
@@ -0,0 +1,16 @@
+﻿'use strict'
+
+define(['app'], function (app) {
+    app.directive('hideInIframe', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+
+                if (self != top)
+                {
+                    element.addClass('ng-hide');
+                }
+            }
+        };
+    }]);
+});
